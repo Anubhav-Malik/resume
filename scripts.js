39c1a9f2454b099bd7877d2cdb4ccbf55107079c
Anubhav-Malik <40168200+Anubhav-Malik@users.noreply.github.com>
2024-06-10 22:12:21 +0530

    Added images

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Example JavaScript effect: Fade in sections
     const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-        section.style.opacity = 0;
-        section.style.transition = 'opacity 1s';
-    });
 
     const fadeInSection = (entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.style.opacity = 1;
+                entry.target.classList.add('visible');
                 observer.unobserve(entry.target);
             }
         });
